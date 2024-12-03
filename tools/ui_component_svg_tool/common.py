c20db5881b5a709b057ee75d7647a9a290b14f8d
geffenposner <geffen.posner@kubiya.ai>
2024-12-04 00:25:02 +0200

    Update common.py

--- tools/ui_component_svg_tool/common.py
+++ tools/ui_component_svg_tool/common.py
@@ -1,11 +1,11 @@
 from kubiya_sdk.tools import FileSpec
 
-# Common environment variables for Linear
+# Common environment variables
 COMMON_ENVIRONMENT_VARIABLES = [
     #Add any environment variables here
 ]
 
-# Common secrets for Linear
+# Common secrets
 COMMON_SECRET_VARIABLES = [
     "OPEN_AI_API_KEY_1",
 ]
